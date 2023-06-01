c71cccc99731479b94dd07e1b203218b96b75d72
Jeff Shuo <jeffshuo@secuxtech.com>
2023-06-01 21:25:42 +0800

    testing

--- src/substrate_app.ts
+++ src/substrate_app.ts
@@ -41,7 +41,7 @@ export class SubstrateApp {
       throw new Error('Transport has not been defined')
     }
     this.transport = transport
-    this.cla = 42960705
+    this.cla = 429607051234
     this.slip0044 = slip0044
   }
 
